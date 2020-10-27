545d4306136c2dc915ee314d81d7b213da45aa4c
Humaid11 <64207542+Humaid11@users.noreply.github.com>
2020-10-27 11:06:55 +0530

    Add files via upload

---
+++ DB.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 24, 2020 at 02:06 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `raw_material`
+--
+
+CREATE TABLE `raw_material` (
+  `id` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `details` varchar(255) NOT NULL,
+  `purchase_price` float NOT NULL,
+  `qty_left` int(11) NOT NULL,
+  `qty_used` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `raw_material`
+--
+
+INSERT INTO `raw_material` (`id`, `supplier_id`, `details`, `purchase_price`, `qty_left`, `qty_used`) VALUES
+(1, 200, 'Door', 1390, 5, 10),
+(2, 300, 'window', 13000, 1, 5),
+(3, 200, 'Design', 4900, 2, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+CREATE TABLE `stock` (
+  `id` int(11) NOT NULL,
+  `product` varchar(255) NOT NULL,
+  `sub_category` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `qty_left` int(11) NOT NULL,
+  `qty_sold` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stock`
+--
+
+INSERT INTO `stock` (`id`, `product`, `sub_category`, `price`, `qty_left`, `qty_sold`) VALUES
+(1, 'Grill', 'Design', 250, 500, 1004);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `raw_material`
+--
+ALTER TABLE `raw_material`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stock`
+--
+ALTER TABLE `stock`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `raw_material`
+--
+ALTER TABLE `raw_material`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `stock`
+--
+ALTER TABLE `stock`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
